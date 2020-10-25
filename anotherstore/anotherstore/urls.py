324be42588b4aecc8ab1e802c4c4e3c845344e30
a-telier <Alexandra.rabi@outlook.com>
2020-10-25 17:04:30 +0000

    first app: product is added and folder structure is updated

--- anotherstore/anotherstore/urls.py
+++ anotherstore/anotherstore/urls.py
@@ -1,4 +1,4 @@
-"""store URL Configuration
+"""anotherstore URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,16 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
-    path('products/', include('products.urls')),
-    path('bag/', include('bag.urls')),
-    path('checkout/', include('checkout.urls')),
-    path('profile/', include('profiles.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
