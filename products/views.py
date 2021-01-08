6a87d0acc9564d7940ba5dcc49683ea6e0f76e95
a-telier <Alexandra.rabi@outlook.com>
2021-01-08 11:54:50 +0000

    added products from the same category, and categories to product_detail page, also added product count to products page

--- products/views.py
+++ products/views.py
@@ -3,15 +3,36 @@ from django.db.models import Q
 from django.contrib import messages
 from .models import Product, Category
 
+from django.db.models.functions import Lower
+
 # Create your views here.
 
 #   This view allows you to see all products
 def all_products(request):
     products = Product.objects.all()
+    all_categories = Category.objects.all()
     query = None
     categories = None
- 
+    sort = None
+    direction = None
+
     if request.GET:
+        #   Sorting by
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        #   Category filtering
         if 'category' in request.GET:
             # spslit into a list by commas
             # use the list to filter the current queries
@@ -20,6 +41,7 @@ def all_products(request):
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
+        #   Search function
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -32,10 +54,14 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(brand__icontains=query) | Q(category__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
+        'all_categories': all_categories,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, "products/products.html", context)
@@ -44,11 +70,13 @@ def all_products(request):
 #   This view allows you to view each product individually
 def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
-    categories = Category.objects.all()
+    products = Product.objects.all()
+    all_categories = Category.objects.all()
 
     context = {
         'product': product,
-        'categories': categories,
+        'products': products,
+        'all_categories': all_categories,
     }
 
     return render(request, "products/product_detail.html", context)
