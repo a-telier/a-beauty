f4ac4d7aaf3cfd0b99d3dd10668056df33515cd1
a-telier <Alexandra.rabi@outlook.com>
2021-01-16 20:40:09 +0000

    modify context.py to add products to cart

--- cart/context.py
+++ cart/context.py
@@ -1,11 +1,25 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import render, get_object_or_404, redirect
+from products.models import Product
+
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
     product_count = []
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total = quantity * product.price
+        product_count = quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -24,7 +38,6 @@ def cart_contents(request):
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
-
     }
 
     return context
--- home/views.py
+++ home/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect  #  renders HTML templates
 from django.http import HttpResponse #  displays HttpResponse
 from django.shortcuts import render
-
 from products.models import Product
 
 # Here are the views that we created.
