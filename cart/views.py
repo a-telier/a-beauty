e6221ddfa87a9a1d021cbc3c1f1d2efdb3585d55
a-telier <Alexandra.rabi@outlook.com>
2021-01-17 14:39:15 +0000

    added redirect to product link from cart_items

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect  #  renders HTML templates
 from django.http import HttpResponse #  displays HttpResponse
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 def view_cart(request):
