972d41b6a8a926cff0cbb238a7f2aca7348f4d7c
a-telier <Alexandra.rabi@outlook.com>
2021-01-19 21:27:49 +0000

    Created templatestags folder incl tools.py containing subtotals filter, which is now incl in /cart template"

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404, redirect  #  renders HTML templates
+from django.shortcuts import render, redirect, reverse  #  renders HTML templates
 from django.http import HttpResponse #  displays HttpResponse
 from django.shortcuts import render
 from products.models import Product
+
 
 # Create your views here.
 def view_cart(request):
@@ -21,3 +22,30 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def update_cart(request, item_id):
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+
+    try:
+        cart = request.session.get('cart', {})
+        
+        cart.pop(item_id)
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+        
+    except Exception as e:
+        return HttpResponse(status=500)
