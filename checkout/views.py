ec9d7fa22a8bf232ad3d2fbd2fc77087611123ce
a-telier <Alexandra.rabi@outlook.com>
2021-01-23 22:33:41 +0000

    added Stripe API variables to settings, and implement them in checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, reverse, redirect
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
@@ -9,7 +10,7 @@ from products.models import Product
 from profiles.models import UserProfile
 # from profiles.forms import UserProfileForm
 
-# from cart.contexts import cart_contents
+from cart.context import cart_contents
 
 import stripe
 import json
@@ -17,16 +18,29 @@ import json
 # Create your views here.
 
 def checkout (request):
+    STRIPE_PUBLIC_KEY = settings.STRIPE_PUBLIC_KEY
+    STRIPE_SECRET_KEY = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "Hold your horses, there is nothing on your cart right now")
         return redirect(reverse('products'))
 
+    current_cart = cart_contents(request)
+    grand_total = current_cart['grand_total']
+    stripe_total = round(grand_total * 100)
+    stripe.api_key = STRIPE_SECRET_KEY
+
+    stripe.PaymentIntent.create(
+        amount = stripe_total,
+        currency = settings.STRIPE_CURR,
+    )
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form' : order_form,
-        'stripe_public_key': 'pk_test_51ICsfUDzMqC2yDSdRI8QLg3Z3pfj68JeCtc4VHd0Yu4IV1jZ0wWBMPbetHiiRYJCox3oBx6Yv3G9tW0ErlF6YPUp00MySU6XSI',
-        'client_secret': 'test client secret',
+        'stripe_public_key': STRIPE_PUBLIC_KEY,
+        'client_secret': intent.client_secret ,
     }
     return render(request, template, context)
