2759e46b888c5d8d2be7c25290af361b44186e0b
a-telier <Alexandra.rabi@outlook.com>
2021-01-23 15:05:31 +0000

    added forms.py to checkout app including OrderForm class

---
+++ checkout/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import Order
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number',
+                  'address', 'city', 'postcode', 'country')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        #   Add placeholders and classes
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'postcode': 'Postal Code',
+            'city': 'City',
+            'address': 'Address',
+        }
+
+        #   setting autofocus to true
+        #   cursor starts in full_name when page is loaded
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        #   Add a star if this is a required field
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+                self.fields[field].label = False
