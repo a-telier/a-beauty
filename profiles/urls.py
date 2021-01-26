300060f4e9eee5cbcffdb1807821164daffd7b34
a-telier <Alexandra.rabi@outlook.com>
2021-01-26 22:45:43 +0000

    basic views and redirecting created for profile app as well as base html template

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-# from .webhooks import webhook
+
 
 urlpatterns = [
-    path('profile', views.profile, name='profile'),
+    path('', views.profile, name='profile'),
     ]
+
