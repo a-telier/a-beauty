25607b91f0629d9c972433377541d5a87f580a1d
a-telier <Alexandra.rabi@outlook.com>
2021-01-29 21:13:54 +0000

    modified the fields in the blog model

--- blog/admin.py
+++ blog/admin.py
@@ -1,19 +1,18 @@
 from django.contrib import admin
-from .models import Article 
+from .models import Article, Blog_category
 
 # Register your models here.
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'teaser', 'status','created_on')
+    list_display = ('title', 'teaser', 'status', 'created_on')
     list_filter = ("status",)
     search_fields = ['title', 'content']
     prepopulated_fields = {'teaser': ('title',)}
   
 class CategoryAdmin (admin.ModelAdmin):
     list_display = (
-        'blog_category',
-        'title',
-        'image',
+        'name',
+        'display_name',
     )
 
 admin.site.register(Article, ArticleAdmin)
--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('all_articles/', views.all_articles, name='all_articles'),
+    path('article/<slug:slug>', views.article_detail, name='article_detail'),
 ]
 
